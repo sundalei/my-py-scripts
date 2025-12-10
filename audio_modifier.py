58889fb67676d0b13587af792c48f096b8c8e6c4
sundalei <sundalei1988@gmail.com>
2025-12-10 21:33:17 +0800

    style: add blank line after module docstring

--- audio_modifier.py
+++ audio_modifier.py
@@ -1,8 +1,9 @@
 """
 Audio Modifier - A tool for reading and modifying FLAC audio file metadata
 """
-from mutagen.flac import FLAC
+
 from mutagen import MutagenError
+from mutagen.flac import FLAC
 
 FLAC_PATH = "/Users/sundalei/Downloads/Taylor Swift_1989.02.Blank Space.flac"
 
