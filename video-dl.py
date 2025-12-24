6bdae35fcf22859032dc9c5cabd10b08f79d54d0
sundalei <sundalei1988@gmail.com>
2025-12-24 23:24:36 +0800

    feat: Externalize dynamic rules to a JSON file and add initial media processing and download logic.

--- video-dl.py
+++ video-dl.py
@@ -38,6 +38,9 @@ USER_AGENT = config["user_agent"]
 X_BC = config["x_bc"]
 SESSION_COOKIE = config["sess"]
 
+with open("dynamic_rules.json", "r") as file:
+    dynamic_rules = json.load(file)
+
 # 0 = do not print file names or api calls
 # 1 = print file names only when max_age is set
 # 2 = always print file names
@@ -45,7 +48,7 @@ SESSION_COOKIE = config["sess"]
 # 4 = print skipped files that already exist
 VERBOSITY = 2
 # Download Directory. Use CMD if null
-DOWNLOAD_DIR = "/Users/sundalei/Downloads"
+DOWNLOAD_DIR = ""
 # List of accounts to skip
 SKIP_ACCOUNTS: list[str] = []
 
@@ -211,6 +214,11 @@ def get_subscriptions():
     return [row["username"] for row in subs]
 
 
+def download_media():
+    """Download media"""
+    pass
+
+
 def get_content(media_type, api_location):
     """Get content"""
     posts = api_request(api_location, media_type)
@@ -222,7 +230,23 @@ def get_content(media_type, api_location):
         posts = post["list"]
 
     if len(posts) > 0:
-        pass
+        print("Found " + str(len(posts)) + " " + media_type)
+        for post in posts:
+            if "media" not in post or ("canViewMedia" in post and not post["canViewMedia"]):
+                continue
+            if "postedAt" in post:
+                post_date = str(post["postedAt"][:10])
+            elif "createdAt" in post:
+                post_date = str(post["createdAt"][:10])
+            else:
+                post_date = "1970-01-01"
+            if len(post["media"]) > 1:
+                album = str(post["id"])
+            else:
+                album = ""
+            for media in post["media"]:
+                if "files" in media and "canView" in media and media["canView"]:
+                    download_media()            
 
     return posts
 
@@ -242,54 +266,14 @@ if __name__ == "__main__":
         except OSError:
             print("Unable to use DOWNLOAD_DIR: " + DOWNLOAD_DIR)
     print("Download directory: " + os.getcwd())
-    # Rules for the signed headers
-    dynamic_rules = {
-        "format": "51892:{}:{:x}:69406376",
-        "app_token": "33d57ade8c02dbc5a333db99ff9ae26a",
-        "static_param": "7HMjX3tp4B4JJDOryHAMCUIQCtmGq69D",
-        "remove_headers": ["user-id"],
-        "checksum_indexes": [
-            15,
-            35,
-            3,
-            7,
-            21,
-            26,
-            39,
-            35,
-            4,
-            0,
-            6,
-            29,
-            35,
-            28,
-            37,
-            27,
-            22,
-            4,
-            9,
-            10,
-            37,
-            21,
-            27,
-            13,
-            17,
-            31,
-            28,
-            24,
-            0,
-            14,
-            9,
-            0,
-        ],
-        "checksum_constant": 53,
-    }
+
     PROFILE_LIST = sys.argv
     PROFILE_LIST.pop(0)
 
     if PROFILE_LIST[-1] == "0":
         LATEST = 1
         PROFILE_LIST.pop(-1)
+
     if len(PROFILE_LIST) > 1 and PROFILE_LIST[-1].isnumeric():
         MAX_AGE = int((datetime.today() - timedelta(int(PROFILE_LIST.pop(-1)))).timestamp())
         from_time = datetime.fromtimestamp(int(MAX_AGE), tz=timezone.utc)
